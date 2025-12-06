e1ee90a1e58b40d1576c281e44d0e1319619c3eb
okan <okansahinfed@gmail.com>
2025-12-06 12:58:10 +0300

    style: load new font

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./app/routes.ts";
 import "./app/i18n";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 async function enableMocking() {
   if (import.meta.env.DEV) {
